661c698786ac3908ba521cabcb3941bf24f2bce6
Mason Speck <mcs.5299@gmail.com>
2024-11-09 13:20:15 -0500

    remade SharedMemory

--- README.md
+++ README.md
@@ -2,80 +2,9 @@
 
 ## SharedMemory
 
-A header-only SharedMemory wrapper. Acquires SharedMemory handle from OS upon construction and closes SharedMemory handle upon destruction.
-
-Two main modes:
-
-- Untyped (default/void template)
-  - Constructor supports arbitrary size
-  - Direct access to raw memory
-- Typed
-  - Automatically allocates only enough space for the template type
-  - Supports implicit casting to template type
-  - operator-> overloaded for direct member access on types with members
-  - Still supports direct access to raw memory
-
-TODO
-
-- Linux Implementation
-- Extra functionality baked into SharedMemory allocation
-  - refcounting: lookup number of SharedMemory handles opened on the current key
-  - NamedMutex: Implement Windows/Linux interprocess mutex class then use it to allow for lock()/unlock()/trylock() impl
-
-Currently only implemented for Windows, Linux support soon.
-
-### Untyped
+A header-only RAII implementation of an OS interprocess shared memory datatype. Uses a key and a 64bit uint to open a mapped piece of shared memory in either Windows or Linux. Provides a simple interface with just two getters:
 
 ```cpp
-std::string key = "mem";
-uint32_t size = 128;
-SharedMemory<> shm(key, size);
-
-void* data = shm.data();         // Pointer to shared memory data block
-uint32_t dataSize = shm.size();  // dataSize == 128
-std::string dataKey = shm.key(); // dataKey == "mem"
-```
-
-### Typed
-
-```cpp
-std::string key = "mem";
-uint32_t size = 128;
-SharedMemory<uint32_t> shm(key);
-
-uint32_t* data = shm.data();     // Pointer to shared memory data block
-uint32_t dataSize = shm.size();  // dataSize == sizeof(uint32_t)
-std::string dataKey = shm.key(); // dataKey == "mem"
-```
-
-### Usage examples
-
-```cpp
-struct Pos {
-    uint32_t x;
-    uint32_t y;
-    uint32_t z;
-};
-
-SharedMemory<uint32_t> shm1("mem1");
-SharedMemory<uint32_t> shm2("mem1"); // shm1 and shm2 now refer to the same memory.
-SharedMemory<Pos>      shm3("mem3");
-SharedMemory<>         shm4("mem4", 16);
-
-// Check if SharedMemory opened by comparing data pointer with nullptr
-if (shm4.data() == nullptr) {
-    std::cout << "Shared memory isn't open!" << std::endl;
-}
-
-// Assignment matching template type are overloaded to be passed through to the underlying datatype
-shm1 = 2;
-shm3 = { 2, 3, 4 };
-
-// Implicit casting to underlying datatype is supported.
-int b = 3 + shm2;
-std::cout << shm2 << std::endl;
-
-// Arrow operator has been overloaded to allow direct access to underlying datatype (provided it is a type with members)
-// Not sure yet if this is good design... but it's neat you can do it!
-std::cout << shm3->x << std::endl;
+void data() { return pData; }
+size_t size() const { return sizeBytes; }
 ```
--- SharedMemory/SharedMemory.h
+++ SharedMemory/SharedMemory.h
@@ -1,397 +1,119 @@
-#ifndef SHARED_MEMORY_H
-#define SHARED_MEMORY_H
+#ifndef CPPUTILS_SHARED_MEMORY_H
+#define CPPUTILS_SHARED_MEMORY_H
 
-#include <string>
 #include <iostream>
-#include <optional>
+#include <string>
+#include <cstdint>
 
-#include "../InterprocessNamedMutex/InterprocessNamedMutex.h"
-
-// Imports and SharedMemoryHandle struct are defined per-platform
-struct SharedMemoryHandle;
-#ifdef _WIN32
-    #define WIN32_LEAN_AND_MEAN
+#if defined(_WIN32)
     #include <windows.h>
-
-    struct SharedMemoryHandle
-    {
-        HANDLE hMapFile = NULL;
-        void* pData = nullptr;
-    };
-#elif defined(__linux__)
-
-    struct SharedMemoryHandle
-    {
-        void* pData = nullptr;
-    };
+#elif defined (__linux__)
+    // TODO:: Linux Implementation
 #else
-#error Unsupported Platform
+    #error SharedMemory.h: Unsupported Platform
 #endif
 
-// Forward declare functions which will be implemented per-platform at end of file
-void platformAcquireSharedMemory(const std::string& key, const uint32_t sizeBytes, SharedMemoryHandle& sharedMemoryHandle);
-void platformReleaseSharedMemory(SharedMemoryHandle& smh);
-
-//#########################################################
-// SharedMemory Generic Template
-//#########################################################
-template <typename _DataType = void>
 class SharedMemory
 {
 public:
-    //---------------------------------------------------------
-    // Constructor
-    //---------------------------------------------------------
-    SharedMemory(const std::string& key)
+    SharedMemory(const std::string& key, size_t sizeBytes)
+        : key(key),
+          sizeBytes(sizeBytes)
     {
-        // Initialize Members
-        m_DataKey = key;
-        m_DataSize = sizeof(_DataType);
-        m_SharedMemoryHandle = SharedMemoryHandle();
-        m_InterprocessNamedMutex.emplace(key + "_mutex");
-
-        // Initialize Based on Members
-        platformAcquireSharedMemory(m_DataKey, m_DataSize, m_SharedMemoryHandle);
+        openSharedMemory();
     }
 
-    //---------------------------------------------------------
-    // Destructor
-    //---------------------------------------------------------
     ~SharedMemory()
     {
-        platformReleaseSharedMemory(m_SharedMemoryHandle);
+        closeSharedMemory();
     }
 
-    //---------------------------------------------------------
-    // Copy Constructor
-    //---------------------------------------------------------
-    SharedMemory(const SharedMemory<_DataType>& other)
-    {
-        // Initialize Members
-        m_DataKey = other.m_DataKey;
-        m_DataSize = sizeof(_DataType);
-        m_SharedMemoryHandle = SharedMemoryHandle();
-        m_InterprocessNamedMutex.emplace(other.m_DataKey + "_mutex");
+    // No copy operations for now
+    SharedMemory(const SharedMemory& other) = delete;
+    SharedMemory& operator=(const SharedMemory& other) = delete;
 
-        // Initialize Based on Members
-        platformAcquireSharedMemory(m_DataKey, m_DataSize, m_SharedMemoryHandle);
+    // No move operations for now
+    SharedMemory(SharedMemory&& other) noexcept = delete;
+    SharedMemory& operator=(SharedMemory&& other) noexcept = delete;
 
-        // No need to copy underlying data since we're opening the same shared memory address.
-    }
-
-    //---------------------------------------------------------
-    // Move Constructor
-    //---------------------------------------------------------
-    SharedMemory(SharedMemory<_DataType>&& other) noexcept
-    {
-        // Initialize Members
-        m_DataKey = other.m_DataKey;
-        m_DataSize = sizeof(_DataType);
-        m_SharedMemoryHandle = other.m_SharedMemoryHandle;
-        m_InterprocessNamedMutex = other.m_InterprocessNamedMutex;
-
-        // Clean up other class
-        other.m_DataKey = "";
-        other.m_DataSize = 0;
-        other.m_SharedMemoryHandle = SharedMemoryHandle();
-        m_InterprocessNamedMutex = std::move(other.m_InterprocessNamedMutex);
-
-        // No need to initialize shared memory since we take control of other's handles.
-    }
-
-    //---------------------------------------------------------
-    // Copy Assignment
-    //---------------------------------------------------------
-    SharedMemory<_DataType>& operator=(const SharedMemory<_DataType>& other)
-    {
-        if (this != &other) {
-            // Clean up our resources
-            platformReleaseSharedMemory(m_SharedMemoryHandle);
-
-            // Initialize Members
-            m_DataKey = other.m_DataKey;
-            m_InterprocessNamedMutex.emplace(other.m_DataKey + "_mutex");
-
-            // Initialize Based on Members
-            platformAcquireSharedMemory(m_DataKey, m_DataSize, m_SharedMemoryHandle);
-
-            // No need to copy underlying data since we're opening the same shared memory address.
-        }
-
-        return *this;
-    }
-
-    //---------------------------------------------------------
-    // Move Assignment
-    //---------------------------------------------------------
-    SharedMemory<_DataType>& operator=(SharedMemory<_DataType>&& other) noexcept
-    {
-        if (this != &other) {
-            // Clean up our resources
-            platformReleaseSharedMemory(m_SharedMemoryHandle);
-
-            // Initialize Members
-            m_DataKey = other.m_DataKey;
-            m_SharedMemoryHandle = other.m_SharedMemoryHandle;
-            m_InterprocessNamedMutex = std::move(other.m_InterprocessNamedMutex);
-
-            // Clean up other class
-            other.m_DataKey = "";
-            other.m_DataSize = 0;
-            other.m_SharedMemoryHandle = SharedMemoryHandle();
-            
-            // No need to initialize shared memory since we take control of other's handles.
-        }
-
-        return *this;
-    }
-
-    //---------------------------------------------------------
-    // _DataType Assignment
-    //---------------------------------------------------------
-    SharedMemory<_DataType>& operator=(const _DataType& other)
-    {
-        *static_cast<_DataType*>(m_SharedMemoryHandle.pData) = other;
-        return *this;
-    }
-
-    //---------------------------------------------------------
-    // operator->
-    //---------------------------------------------------------
-    _DataType* operator->() const
-    {
-        return static_cast<_DataType*>(m_SharedMemoryHandle.pData);
-    }
-
-    //---------------------------------------------------------
-    // operator cast to _DataType
-    //---------------------------------------------------------
-    operator _DataType() const
-    {
-        return *static_cast<_DataType*>(m_SharedMemoryHandle.pData);
-    }
-
-    // Returns pointer to raw data, pretyped to template type
-    _DataType* data()     { return static_cast<_DataType*>(m_SharedMemoryHandle.pData); }
-    // Returns the key used to open this instance of shared memory
-    std::string key()     { return m_DataKey; }
-    // Returns size of SharedMemory in bytes
-    uint32_t size()       { return m_DataSize; }
-
-    void lock()
-    {
-        m_InterprocessNamedMutex.value().lock();
-    }
-
-    void unlock()
-    {
-        m_InterprocessNamedMutex.value().unlock();
-    }
-
-    bool tryLock()
-    {
-        return m_InterprocessNamedMutex.value().tryLock();
-    }
+    void* data() { return pData; }
+    size_t size() const { return sizeBytes; }
 
 private:
-    std::string               m_DataKey            {};
-    uint32_t                  m_DataSize           { 0 };
-    SharedMemoryHandle        m_SharedMemoryHandle {};
-    std::optional<InterprocessNamedMutex> m_InterprocessNamedMutex;
-};
+    void* pData{ nullptr };
+    std::string key;
+    size_t sizeBytes{ 0 };
 
-//#########################################################
-// SharedMemory Void Template
-//#########################################################
-template <>
-class SharedMemory<>
-{
-public:
-    //---------------------------------------------------------
-    // Constructor
-    //---------------------------------------------------------
-    SharedMemory(const std::string& key, const uint32_t sizeBytes)
-    {
-        // Initialize Members
-        m_DataKey = key;
-        m_DataSize = sizeBytes;
-        m_SharedMemoryHandle = SharedMemoryHandle();
-
-        // Initialize Based on Members
-        platformAcquireSharedMemory(m_DataKey, m_DataSize, m_SharedMemoryHandle);
-    }
-
-    //---------------------------------------------------------
-    // Destructor
-    //---------------------------------------------------------
-    ~SharedMemory()
-    {
-        platformReleaseSharedMemory(m_SharedMemoryHandle);
-    }
-
-    //---------------------------------------------------------
-    // Copy Constructor
-    //---------------------------------------------------------
-    SharedMemory(const SharedMemory<void>& other)
-    {
-        // Initialize Members
-        m_DataKey = other.m_DataKey;
-        m_DataSize = other.m_DataSize;
-        m_SharedMemoryHandle = SharedMemoryHandle();
-
-        // Initialize Based on Members
-        platformAcquireSharedMemory(m_DataKey, m_DataSize, m_SharedMemoryHandle);
-
-        // No need to copy underlying data since we're opening the same shared memory address.
-    }
-
-    //---------------------------------------------------------
-    // Move Constructor
-    //---------------------------------------------------------
-    SharedMemory(SharedMemory<void>&& other) noexcept
-    {
-        // Initialize Members
-        m_DataKey = other.m_DataKey;
-        m_DataSize = other.m_DataSize;
-        m_SharedMemoryHandle = other.m_SharedMemoryHandle;
-
-        // Clean up other class
-        other.m_DataKey = "";
-        other.m_DataSize = 0;
-        other.m_SharedMemoryHandle = SharedMemoryHandle();
-
-        // No need to initialize shared memory since we take control of other's handles.
-    }
-
-    //---------------------------------------------------------
-    // operator= Copy
-    //---------------------------------------------------------
-    SharedMemory<void>& operator=(const SharedMemory<void>& other)
-    {
-        // Clean up our resources
-        platformReleaseSharedMemory(m_SharedMemoryHandle);
-
-        // Initialize Members
-        m_DataKey = other.m_DataKey;
-        m_DataSize = other.m_DataSize;
-
-        // Initialize Based on Members
-        platformAcquireSharedMemory(m_DataKey, m_DataSize, m_SharedMemoryHandle);
-
-        // No need to copy underlying data since we're opening the same shared memory address.
-
-        return *this;
-    }
-
-    //---------------------------------------------------------
-    // operator= Move
-    //---------------------------------------------------------
-    SharedMemory<void>& operator=(SharedMemory<void>&& other) noexcept
-    {
-        if (this != &other) {
-            // Clean up our resources
-            platformReleaseSharedMemory(m_SharedMemoryHandle);
-
-            // Initialize Members
-            m_DataKey = other.m_DataKey;
-            m_DataSize = other.m_DataSize;
-            m_SharedMemoryHandle = other.m_SharedMemoryHandle;
-
-            // Clean up other class
-            other.m_DataKey = "";
-            other.m_DataSize = 0;
-            other.m_SharedMemoryHandle = SharedMemoryHandle();
-
-            // No need to initialize shared memory since we take control of other's handles.
-        }
-
-        return *this;
-    }
-
-    // Returns pointer to raw data, pretyped to template type
-    void* data() { return m_SharedMemoryHandle.pData; }
-    // Returns the key used to open this instance of shared memory
-    std::string key() { return m_DataKey; }
-    // Returns size of SharedMemory in bytes
-    uint32_t size() { return m_DataSize; }
-
-private:
-    std::string               m_DataKey{};
-    uint32_t                  m_DataSize{ 0 };
-    SharedMemoryHandle        m_SharedMemoryHandle{};
-};
-
-#ifdef _WIN32
-//#########################################################
-// Windows Shared Memory Interface
-//#########################################################
-
-//---------------------------------------------------------
-// platformAcquireSharedMemory
-//---------------------------------------------------------
-void platformAcquireSharedMemory(const std::string& key, const uint32_t sizeBytes, SharedMemoryHandle& sharedMemoryHandle)
-{
-    sharedMemoryHandle.hMapFile = CreateFileMappingA(
-        INVALID_HANDLE_VALUE,         // Use paging file
-        NULL,                         // Default security
-        PAGE_READWRITE,               // Read/write access
-        0,                            // Maximum object size (high-order DWORD)
-        sizeBytes,                    // Maximum object size (low-order DWORD)
-        key.c_str());                 // Name of mapping object
-
-    if (sharedMemoryHandle.hMapFile == NULL) {
-        std::cerr << "Could not create file mapping object: " << GetLastError() << std::endl;
-        return;
-    }
-
-    sharedMemoryHandle.pData = MapViewOfFile(
-        sharedMemoryHandle.hMapFile,  // Handle to map object
-        FILE_MAP_ALL_ACCESS,          // Read/write permission
-        0,                            // Offset high
-        0,                            // Offset low
-        sizeBytes);                   // Number of bytes to map
-
-    if (sharedMemoryHandle.pData == nullptr) {
-        std::cerr << "Could not map view of file: " << GetLastError() << std::endl;
-        CloseHandle(sharedMemoryHandle.hMapFile);
-        sharedMemoryHandle.hMapFile = NULL;
-    }
-}
-
-//---------------------------------------------------------
-// platformReleaseSharedMemory
-//---------------------------------------------------------
-void platformReleaseSharedMemory(SharedMemoryHandle& smh)
-{
-    if (smh.pData != nullptr) {
-        UnmapViewOfFile(smh.pData);
-        smh.pData = nullptr;
-    }
-
-    if (smh.hMapFile != NULL) {
-        CloseHandle(smh.hMapFile);
-        smh.hMapFile = NULL;
-    }
-}
-
+    // Platform-specific members
+#if defined(_WIN32)
+    HANDLE hFileMapping{ NULL };
 #elif defined(__linux__)
-//#########################################################
-// Linux Shared Memory Interface
-//#########################################################
-
-//---------------------------------------------------------
-// platformAcquireSharedMemory
-//---------------------------------------------------------
-void platformAcquireSharedMemory(const std::string& key, const uint32_t sizeBytes, SharedMemoryHandle& sharedMemoryHandle)
-{
-}
-
-//---------------------------------------------------------
-// platformReleaseSharedMemory
-//---------------------------------------------------------
-void platformReleaseSharedMemory(SharedMemoryHandle& smh)
-{
-}
+    // TODO:: Linux Implementation
+#else
+    #error SharedMemory.h: Unsupported Platform
 #endif
+
+    void openSharedMemory()
+    {
+#if defined(_WIN32)
+        uint32_t lower = static_cast<uint32_t>(sizeBytes & 0xFFFFFFFF);
+        uint32_t upper = static_cast<uint32_t>((sizeBytes >> 32) & 0xFFFFFFFF);
+
+        hFileMapping = CreateFileMappingA(
+            NULL,            // File backing the mapping. When NULL, system paging file is used (shared memory)
+            NULL,
+            PAGE_READWRITE,
+            upper,
+            lower,
+            key.c_str()
+        );
+
+        if (hFileMapping == NULL) {
+            std::cerr << "ERROR SharedMemory::openSharedMemory() (Windows) CreateFileMappingA returned NULL, could not open file!" << std::endl;
+            return;
+        }
+
+        pData = MapViewOfFile(
+            hFileMapping,
+            FILE_MAP_ALL_ACCESS,
+            0,
+            0,
+            sizeBytes
+        );
+
+        if (pData == nullptr) {
+            std::cerr << "ERROR SharedMemory::openSharedMemory() (Windows) MapViewOfFile returned nullptr. Could not get pointer to shared memory!" << std::endl;
+            CloseHandle(hFileMapping);
+            hFileMapping = NULL;
+            return;
+        }
+
+#elif defined (__linux__)
+    // TODO:: Linux Implementation
+#else
+    #error SharedMemory: Unsupported Platform
+#endif
+    }
+
+    void closeSharedMemory()
+    {
+#if defined(_WIN32)
+        if (pData != nullptr) {
+            UnmapViewOfFile(pData);
+            pData = nullptr;
+        }
+
+        if (hFileMapping != NULL) {
+            CloseHandle(hFileMapping);
+            hFileMapping = NULL;
+        }
+
+#elif defined (__linux__)
+    // TODO:: Linux Implementation
+#else
+    #error SharedMemory.h: Unsupported Platform
+#endif
+    }
+};
 
 #endif
