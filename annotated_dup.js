f8cc0b9cda745468fa4763d1cb7758ceaf7e660d
StopwatchString <StopwatchString@users.noreply.github.com>
2025-08-15 01:08:53 +0000

    deploy: 44acfb0ba7d50a34695527219ff5f1a3bfd189dd

--- annotated_dup.js
+++ annotated_dup.js
@@ -24,6 +24,7 @@ var annotated_dup =
       [ "is_clock< T, std::void_t< typename T::time_point, decltype(T::now())> >", "structcpputils_1_1is__clock_3_01T_00_01std_1_1void__t_3_01typename_01T_1_1time__point_00_01decltype_07T_1_1now_07_08_08_4_01_4.html", null ],
       [ "is_output_stream", "structcpputils_1_1is__output__stream.html", null ],
       [ "is_output_stream< T, std::void_t< decltype(std::declval< T & >()<< std::declval< long long >())> >", "structcpputils_1_1is__output__stream_3_01T_00_01std_1_1void__t_3_01decltype_07std_1_1declval_3_05b196cd4bef15d81f60bd9cca0daa76f.html", null ],
+      [ "RangeMeter", "structcpputils_1_1RangeMeter.html", null ],
       [ "ResettableTimer", "classcpputils_1_1ResettableTimer.html", null ],
       [ "ScopePrintTimer", "classcpputils_1_1ScopePrintTimer.html", null ],
       [ "SharedLibraryLoader", "classcpputils_1_1SharedLibraryLoader.html", null ],
--- search/classes_3.js
+++ search/classes_3.js
@@ -1,5 +1,6 @@
 var searchData=
 [
-  ['readcontexts_0',['ReadContexts',['../structcpputils_1_1win_1_1ReadContexts.html',1,'cpputils::win']]],
-  ['resettabletimer_1',['ResettableTimer',['../classcpputils_1_1ResettableTimer.html',1,'cpputils']]]
+  ['rangemeter_0',['RangeMeter',['../structcpputils_1_1RangeMeter.html',1,'cpputils']]],
+  ['readcontexts_1',['ReadContexts',['../structcpputils_1_1win_1_1ReadContexts.html',1,'cpputils::win']]],
+  ['resettabletimer_2',['ResettableTimer',['../classcpputils_1_1ResettableTimer.html',1,'cpputils']]]
 ];
